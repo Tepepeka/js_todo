f9fbb081de24fc2993efeb35ebe6bc358b3c7dd6
Tepepeka <jeremy.bourgoin@live.fr>
2023-03-07 17:03:42 +0100

    first part OK

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ import { createElement } from "./functions/dom.js";
 
 try {
 const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
-const list =new TodoList(todos)
+const list = new TodoList(todos)
 list.appendTo(document.querySelector('#todolist'))
 } catch (e) {
-    const alertElement = createElement('div',{
+    const alertElement = createElement('div', {
       class: 'alert alert-danger m-2',
       role: 'alert'
     })
     alertElement.innerText = "Can't load element"
     document.body.prepend(alertElement)
+    console.error(e)
 }
--- components/TodoList.js
+++ components/TodoList.js
@@ -14,12 +14,15 @@ export class TodoList {
   /**@type {Todo[]} */
   #todos = []
 
+  /**@type {HTMLUListElement} */
+  #listElement = []
+
   /**
    * 
    * @param {Todo[]} todos 
    */
   constructor (todos) {
-    this.#todos= todos
+    this.#todos = todos
   }
 
   /**
@@ -27,25 +30,74 @@ export class TodoList {
    * @param {HTMLElement} element
    */
   appendTo (element) {
-    element.innerHTML = `<form class="d-flex pb-4">
-    <input required="" class="form-control" type="text" placeholder="Acheter des patates..." name="title" data-com.bitwarden.browser.user-edited="yes">
-    <button class="btn btn-primary">Ajouter</button>
-</form>
-<main>
-    <div class="btn-group mb-4" role="group">
+    element.innerHTML = 
+    `<form class="d-flex pb-4">
+      <input required="" class="form-control" type="text" placeholder="Acheter des patates..." name="title" data-com.bitwarden.browser.user-edited="yes">
+      <button class="btn btn-primary">Ajouter</button>
+    </form>
+    <main>
+      <div class="btn-group mb-4" role="group">
         <button type="button" class=" btn btn-outline-primary active" data-filter="all">Toutes</button>
         <button type="button" class=" btn btn-outline-primary" data-filter="todo">A faire</button>
         <button type="button" class=" btn btn-outline-primary" data-filter="done">Faites</button>
-    </div>
+      </div>
 
-    <ul class="list-group">
-    </ul>
-</main>`
-const list = element.querySelector('.list-group')
-for (let todo of this.#todos) {
-  const task = new TodoListItem(todo)
-  task.appendTo(list)
-}
+      <ul class="list-group">
+      </ul>
+    </main>`
+
+    this.#listElement = element.querySelector('.list-group')
+    for (let todo of this.#todos) {
+      const task = new TodoListItem(todo)
+      this.#listElement.append(task.element)
+    }
+    element.querySelector('form').addEventListener('submit', e => this.#onSubmit(e))
+    element.querySelectorAll('.btn-group button').forEach(button => {
+      button.addEventListener('click', e => this.#toggleFilter(e))
+    })
+  }
+  /**
+   * 
+   * @param {SubmitEvent} e 
+   */
+  #onSubmit(e){
+    e.preventDefault()
+    const form = e.currentTarget
+    const title = new FormData(form).get('title').toString().trim()
+    //console.log(title);
+    if (title === '') {
+      return
+    }
+    const todo = {
+      id: Date.now(),
+      title,
+      completed: false
+    }
+    const item = new TodoListItem(todo)
+    this.#listElement.prepend(item.element)
+    form.reset()
+  }
+
+  /**
+   * 
+   * @param {PointerEvent} e 
+   */
+  #toggleFilter(e) {
+    e.preventDefault()
+    const filter = e.currentTarget.getAttribute('data-filter')
+    //console.log(filter);
+    e.currentTarget.parentElement.querySelector('.active').classList.remove('active')
+    e.currentTarget.classList.add('active')
+    if (filter === 'todo') {
+      this.#listElement.classList.add('hide-completed')
+      this.#listElement.classList.remove('hide-todo')
+    } else if (filter === 'done'){
+      this.#listElement.classList.add('hide-todo')
+      this.#listElement.classList.remove('hide-completed')
+    } else {
+      this.#listElement.classList.remove('hide-todo')
+      this.#listElement.classList.remove('hide-completed')
+    }
   }
 }
 
@@ -59,10 +111,12 @@ class TodoListItem {
     const li = createElement('li', {
       class: 'todo list-group-item d-flex align-items-center'
     })
+    this.#element = li
     const checkbox = createElement('input', {
       type: 'checkbox',
       class: 'form-check-input',
-      id
+      id,
+      checked: todo.completed ? '' : null
     })
     
     const label = createElement('label', {
@@ -79,14 +133,36 @@ class TodoListItem {
     li.append(checkbox)
     li.append(label)
     li.append(button)
-    this.#element = li
+    this.toggle(checkbox)
+
+    button.addEventListener('click', e => this.remove(e))
+    checkbox.addEventListener('change', e => this.toggle(e.currentTarget))
+
   }
 
   /**
    * 
-   * @param {HTMLElement} element 
+   * @return {HTMLElement} element 
    */
-  appendTo (element) {
-    element.append(this.#element)
+  get element() {
+    return this.#element
+  }
+
+  remove(e) {
+    e.preventDefault()
+    this.#element.remove()
+  }
+
+  /**
+   * cChange l'etat (a faire / fait) de la task
+   * 
+   * @param {HTMLInputElement} checkbox 
+   */
+  toggle(checkbox) {
+    if (checkbox.checked) {
+      this.#element.classList.add('is-completed')
+    } else {
+      this.#element.classList.remove('is-completed')
+    }
   }
 }
